0886723132cd3ac10bc55e93e2c39afdefaf2c62
Bryan Braggion Pimentel de Lima <bryanbraggionpimentel@hotmail.com>
2026-06-01 10:58:43 +0000

    versao 2

--- react_vazio/vazio/App.js
+++ react_vazio/vazio/App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Abra o arquivo App.js para começar a trabalhar no seu app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={stylr}>
+      <Text>Some text</Text>
+      <View>
+        <Text>Abra o arquivo App.js para começar a trabalhar no seu app!</Text>
+      </View>
+    </ScrollView>
   );
 }
 
